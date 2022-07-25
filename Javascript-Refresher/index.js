ceb7ab7993414bcd22017b00ad57a8d7a700f91d
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-07-25 16:30:33 -0500

    Collections

--- Javascript-Refresher/index.js
+++ Javascript-Refresher/index.js
@@ -498,3 +498,82 @@ function checkObj(checkProp){
 
 console.log(checkObj("gift"));
 // --------------------------------------------------------------------------------------
+var myStorage = {
+    "car" : {
+        "inside" : {
+            "glove box" : "maps",
+            "passenger seat" : "crumbs"
+        },
+        "outside" : {
+            "trunk" : "jack"
+        }
+    }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
+
+var myPlants = [
+    {
+        type : "flowers",
+        list : [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type : "trees",
+        list : [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+]
+
+var secondTree = myPlants[1].list[1];
+console.log(secondTree);
+
+var collection = {
+    "2548" : {
+        "album" : "Spliperry When Wet",
+        "artist" : "Bon Jovi",
+        "tracks" : [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468" : {
+        "album" : "1999",
+        "artist" : "Prince",
+        "tracks" : [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245" : {
+        "artist" : "Robert Palmer",
+        "tracks" : []
+    },
+    "5439" : {
+        "album" : "ABBA Gold"
+    }
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value){
+    if(value === ""){
+        delete collection[id][prop];
+    }else if (prop === "tracks"){
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    }else{
+        collection[id][prop] = value;
+    }
+
+    return collection;
+}
+
+console.log(updateRecords(5439,"artist","ABBA"));
