fa4bf1d002c2afd173cbab741df698c6eff141b8
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-07-26 09:54:03 -0500

    Arrow Functions

--- Javascript-Refresher/index.js
+++ Javascript-Refresher/index.js
@@ -636,3 +636,24 @@ function checkSign(n){
 }
 
 console.log(checkSign(0));
+// --------------------------------------------------------------------------------------
+const magic = () => new Date();
+
+console.log(magic);
+
+// var myConcat = function(arr1,arr2){
+//     return arr1.concat(arr2);
+// }
+const myConcat = (arr1,arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1,2],[3,4,5]));
+
+const realNumbersArray = [
+    4,-5.6,-9.8,3.14,42,6,8.34,-2
+];
+
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    return squaredIntegers
+}
+
