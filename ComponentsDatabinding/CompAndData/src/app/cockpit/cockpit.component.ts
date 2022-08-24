4aeef467bece7b8104b4b1d94504328c2785ae6f
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-08-24 08:47:32 -0500

    Databinding Angular

---
+++ ComponentsDatabinding/CompAndData/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, EventEmitter, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+	selector: 'app-cockpit',
+	templateUrl: './cockpit.component.html',
+	styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+	serverCreated = new EventEmitter<{serverName : string, serverContent: string}>();
+	blueprintCreated = new EventEmitter<{serverName : string, serverContent: string}>();
+	// newServerName = '';
+	// newServerContent = '';
+	@ViewChild('serverContentInput', { static: true })
+	serverContentInpunt!: ElementRef;
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	onAddServer(){
+		// this.serverCreated.emit({
+		// 	serverName: this.newServerName,
+		// 	serverContent: this.newServerContent
+		// });
+	}
+
+	onAddBlueprint(){
+		// this.blueprintCreated.emit({
+		// 	serverName: this.newServerName,
+		// 	serverContent: this.newServerContent
+		// });
+	}
+}
